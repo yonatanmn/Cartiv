139c6b81f60404ca6b077fa08a23b724654c38fd
yonatan s <yonatanmn@gmail.com>
2016-02-17 18:48:24 +0200

    add all actions

--- examples/todomvc/stores/todoStore.js
+++ examples/todomvc/stores/todoStore.js
@@ -28,13 +28,59 @@ export default createStore({api, name: 'todo'}, {
   },
 
   onChangeFilter(filter){
-    this.setState({ filter })
+    this.setState({filter})
   },
+
+  onDelete(id){
+    this.setState({
+      todos: this.state.todos.filter(todo =>
+        todo.id !== id
+      )
+    })
+  },
+
+  onEdit(id, text){
+    this.setState({
+      todos: this.state.todos.map(todo =>
+        todo.id === id ?
+          Object.assign({}, todo, {text}) :
+          todo
+      )
+    })
+  },
+
+  onComplete(id){
+    this.setState({
+      todos: this.state.todos.map(todo =>
+        todo.id === id ?
+          Object.assign({}, todo, {completed: !todo.completed}) :
+          todo
+      )
+    })
+  },
+
+  onCompleteAll(){
+    let {todos} = this.state;
+    const areAllMarked = todos.every(todo => todo.completed);
+    this.setState({
+      todos: todos.map(todo => Object.assign({}, todo, {
+        completed: !areAllMarked
+      }))
+    })
+  },
+
+
+  onClearCompleted(){
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.completed === false)
+    })
+  },
+
 
   storeDidUpdate(prevState){
     let {todos} = this.state;
 
-    if(prevState.todos !== todos){
+    if (prevState.todos !== todos) {
       const completedCount = todos.reduce((count, todo) =>
           todo.completed ? count + 1 : count,
         0
