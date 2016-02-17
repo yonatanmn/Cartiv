24fc13dce97a16f153080a5c726e5deabb37f20b
yonatan s <yonatanmn@gmail.com>
2016-02-17 16:24:04 +0200

    clean index

--- examples/todomvc/index.js
+++ examples/todomvc/index.js
@@ -1,16 +1,11 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 import App from './components/App'
-import configureStore from './store/configureStore'
 import 'todomvc-app-css/index.css'
 
-const store = configureStore()
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root')
 )
