530da71de19261680f92f4e2f106c8d0c2d9545a
yonatan s <yonatanmn@gmail.com>
2016-02-17 15:58:39 +0200

    config changes

--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -34,21 +34,21 @@ module.exports = {
   }
 }
 
-//
-//// When inside Redux repo, prefer src to compiled version.
-//// You can safely delete these lines in your project.
-//var reduxSrc = path.join(__dirname, '..', '..', 'src')
-//var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules')
-//var fs = require('fs')
-//if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
-//  // Resolve Redux to source
-//  module.exports.resolve = { alias: { 'redux': reduxSrc } }
-//  // Our root .babelrc needs this flag for CommonJS output
-//  process.env.BABEL_ENV = 'commonjs'
-//  // Compile Redux from source
-//  module.exports.module.loaders.push({
-//    test: /\.js$/,
-//    loaders: [ 'babel' ],
-//    include: reduxSrc
-//  })
-//}
+
+// When inside cartiv repo, prefer src to compiled version.
+// You can safely delete these lines in your project.
+var cartivSrc = path.join(__dirname, '..', '..', 'src')
+var cartivNodeModules = path.join(__dirname, '..', '..', 'node_modules')
+var fs = require('fs')
+if (fs.existsSync(cartivSrc) && fs.existsSync(cartivNodeModules)) {
+  // Resolve cartiv to source
+  module.exports.resolve = { alias: { 'cartiv': cartivSrc } }
+  // Our root .babelrc needs this flag for CommonJS output
+  process.env.BABEL_ENV = 'commonjs'
+  // Compile cartiv from source
+  module.exports.module.loaders.push({
+    test: /\.js$/,
+    loaders: [ 'babel' ],
+    include: cartivSrc
+  })
+}
