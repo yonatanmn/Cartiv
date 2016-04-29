a5e9767f67a46eebe51dd779ae474e41884dfae9
Yonatan <yonatanmn@gmail.com>
2016-04-30 00:30:37 +0300

    store tests

--- test/createStore.spec.js
+++ test/createStore.spec.js
@@ -56,7 +56,7 @@ describe('Cartiv Store', () => {
     });
   });
 
-  describe('created Store', () => {
+  describe('created Store and subscriptions', () => {
     let api;
     let dispatcherConfig;
     let basicStoreDef = {
@@ -92,29 +92,103 @@ describe('Cartiv Store', () => {
       expect(store.subscriptions.length).to.eql(2);
       expect(store.subscriptions[0].listenable.actionName).to.eql('onActionA');
     });
+
+    it('should listen to actions', (done) => {
+      basicStoreDef.onActionA = () => { done(); };
+      /*let store = */createStore(dispatcherConfig, basicStoreDef);
+      api.name.onActionA();
+    });
   });
 
   describe('state changes', () => {
-    let api;
-    let dispatcherConfig;
+    let api = createAPI();
+    let dispatcherConfig = { api, name: 'name' };
+    let store;
     let basicStoreDef = {
-      //getInitialState() { return { a: 1 }; },
-      //a: emptyFunction, b: emptyFunction,
-      //onActionA: emptyFunction, onActionB: emptyFunction
+      getInitialState() { return { a: 1, b: 'b', c: { d: 'c.d' } }; },
     };
+
+    beforeEach(() => {
+      store = createStore(dispatcherConfig, basicStoreDef);
+    });
+
+    it('should have state based on getInitialState', () => {
+      expect(store.state.a).to.eql(1);
+    });
+
+    it('should change state based on setState', () => {
+      store.setState({ a: 2 });
+      expect(store.state.a).to.eql(2);
+      expect(store.state.b).to.eql('b');
+      store.setState({ c: { e: 'c.e' } });
+      expect(store.state.c.d).to.be(undefined);
+      expect(store.state.c.e).to.eql('c.e');
+    });
+  });
+
+  describe('storeDidUpdate, shouldStoreUpdate', () => {
+    let api;// = createAPI();
+    let dispatcherConfig;// = { api, name: 'name' };
+    let store;
+    let basicStoreDef;
 
     beforeEach(() => {
       api = createAPI();
       dispatcherConfig = { api, name: 'name' };
+      basicStoreDef = {
+        getInitialState() {
+          return { a: 1 };
+        },
+      };
     });
 
-    it('should have state based on getInitialState', () => {
-      basicStoreDef.getInitialState = () => { return { a: 1 }; };
-
-      let store = createStore(dispatcherConfig, basicStoreDef);
-
-      expect(store.state).to.eql({ a: 1 });
+    it('should call storeDidUpdate after setState', (done) => {
+      basicStoreDef.storeDidUpdate = (prevState) => {
+        expect(prevState.a).to.eql(1);
+        expect(store.state.a).to.eql(2);
+        done();
+      };
+      store = createStore(dispatcherConfig, basicStoreDef);
+      store.setState({ a: 2 });
     });
+
+    it('should prevent state change if shouldStoreUpdate returns false', () => {
+      basicStoreDef.shouldStoreUpdate = (newState) => {
+        return newState.a !== 2;
+      };
+      store = createStore(dispatcherConfig, basicStoreDef);
+      store.setState({ a: 2 });
+      expect(store.state.a).to.eql(1);
+    });
+
+    it('should not prevent state change if shouldStoreUpdate returns true', () => {
+      basicStoreDef.shouldStoreUpdate = (newState) => {
+        return newState.a === 2;
+      };
+      store = createStore(dispatcherConfig, basicStoreDef);
+      store.setState({ a: 2 });
+      expect(store.state.a).to.eql(2);
+    });
+  });
+
+  it('should work, full flow - action to state', (done) => {
+    let api = createAPI();
+    let dispatcherConfig = { api, name: 'name' };
+    let basicStoreDef = {
+      getInitialState() { return { a: 1, b: 'b', c: 3 }; },
+      onActionA() {
+        this.setState({ a: 2, b: 1 });
+      }
+    };
+
+    let store = createStore(dispatcherConfig, basicStoreDef);
+
+    expect(store.state).to.eql({ a: 1, b: 'b', c: 3 });
+    api.name.onActionA();
+    setTimeout(() => {
+      expect(store.state).to.eql({ a: 2, b: 1, c: 3 });
+      done();
+    }, 1);
   });
 });
 
